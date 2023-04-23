d023da873d47c651fec9a90d34c12c318c0ac8d4
scout <scout.dks@gmail.com>
2023-04-23 14:27:05 -0600

    Added simple changes to allow build

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -6,49 +6,48 @@ import styles from "@/styles/Home.module.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <>
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12 subtitles">
-            <h1>{"> Contact Me_"}</h1>
-          </div>
-        </div>
-        <div className="row justify-content-between">
-          <div className="col-sm-5 subtitles">
-            <p>
-              If you have any question, please don`t hesitate to contact using
-              form below...
-            </p>
-            <form>
-              <div className="form-group">
-                <div className="form-row">
-                  <div className="col">
-                    {/* <input type="text" className="form-control" placeholder="Name">                                    */}
-                  </div>
-                  <div className="col">
-                    {/* <input type="email" className="form-control" placeholder="Email">              */}
-                  </div>
-                </div>
-              </div>
-              <div className="form-group">
-                {/* <input type="text" className="form-control" placeholder="Title"> */}
-              </div>
-              <div className="form-group">
-                <textarea
-                  type="text"
-                  className="form-control"
-                  placeholder="Message"
-                ></textarea>
-              </div>
-              <div className="titles">
-                <button type="submit">Send</button>
-              </div>
-            </form>
-          </div>
-          <div className="col-sm-6 subtitles"></div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="container">
+				<div className="row">
+					<div className="col-sm-12 subtitles">
+						<h1>{"> Contact Me_"}</h1>
+					</div>
+				</div>
+				<div className="row justify-content-between">
+					<div className="col-sm-5 subtitles">
+						<p>
+							If you have any question, please don`t hesitate to contact using
+							form below...
+						</p>
+						<form>
+							<div className="form-group">
+								<div className="form-row">
+									<div className="col">
+										{/* <input type="text" className="form-control" placeholder="Name">                                    */}
+									</div>
+									<div className="col">
+										{/* <input type="email" className="form-control" placeholder="Email">              */}
+									</div>
+								</div>
+							</div>
+							<div className="form-group">
+								{/* <input type="text" className="form-control" placeholder="Title"> */}
+							</div>
+							<div className="form-group">
+								<textarea
+									className="form-control"
+									placeholder="Message"
+								></textarea>
+							</div>
+							<div className="titles">
+								<button type="submit">Send</button>
+							</div>
+						</form>
+					</div>
+					<div className="col-sm-6 subtitles"></div>
+				</div>
+			</div>
+		</>
+	);
 }
