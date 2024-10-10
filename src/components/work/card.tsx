578296d8a8920830ebf35f2c9d21bfabd9b8569d
Darksw20 <ricardog.navi20@gmail.com>
2024-10-10 14:51:55 -0600

    Fixed issues related to dinamic pages

--- src/components/work/card.tsx
+++ src/components/work/card.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 
 export default function Card({ name, src, description }: any) {
-  return (
-    <div className="column web">
-      <div className="content">
-        <Image src={src} height={100} alt="Port1" width={100} />
-        <h4>{name}</h4>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+	const PUBLIC = process.env.NEXT_PUBLIC_PUBLIC_ENDPOINT;
+	const imageLink = `${PUBLIC}/img/${src}`;
+	return (
+		<div className="column web">
+			<div className="content">
+				<Image src={imageLink} height={100} alt="Port1" width={100} />
+				<h4>{name}</h4>
+				<p>{description}</p>
+			</div>
+		</div>
+	);
 }
