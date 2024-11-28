947169db8dab06fdb54bde749f977a91769a56a7
Darksw20 <ricardog.navi20@gmail.com>
2024-11-27 22:36:45 -0600

    Added username commands

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -5,9 +5,48 @@ import styles from "@/styles/Home.module.css";
 import { Home, Person, Settings, Work, Phone } from "@mui/icons-material";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const ENDPOINT = process.env.NEXT_PUBLIC_BACKEND_ENDPOINT;
+
+interface User {
+	id: number;
+	email: string;
+	username: string;
+	summary: string;
+	motivation: string;
+	github: string;
+	linkedin: string;
+}
+
+const useUserData = (user: string | undefined) => {
+	const [userInfo, setUserInfo] = useState<User | null>(null);
+
+	const fetchData = useCallback(async (user: string) => {
+		try {
+			const [userInfoData] = await Promise.all([getUserInfo(user)]);
+			setUserInfo(userInfoData);
+		} catch (error) {
+			console.error("Error fetching data:", error);
+		}
+	}, []);
+
+	return { userInfo, fetchData };
+};
+
+const getUserInfo = async (user: string): Promise<User | null> => {
+	try {
+		const response = await fetch(`${ENDPOINT}/users?user=${user}`);
+		const data: User[] = await response.json();
+		return data[0] ?? null;
+	} catch (error) {
+		console.error("Error fetching roles:", error);
+		// Handle the error or return a default value
+		return null;
+	}
+};
 
 export default function Navbar() {
 	const router = useRouter();
@@ -15,13 +54,16 @@ export default function Navbar() {
 	console.log("nvar user", user);
 	const [username, setUsername] = useState(user);
 
+	const { userInfo, fetchData } = useUserData(user);
+
 	useEffect(() => {
 		if (router.isReady) {
 			const profile =
 				user || (process.env.NEXT_PUBLIC_DEFAULT_PROFILE as string);
 			setUsername(profile);
+			fetchData(profile);
 		}
-	}, [router.isReady, user]);
+	}, [router.isReady, user, fetchData]);
 
 	return (
 		<aside className="container py-6 flex flex-col sidebar bg-neutral-900 grid-rows-3 justify-between w-16 h-screen sticky top-0 ">
@@ -46,25 +88,29 @@ export default function Navbar() {
 				</Link>
 			</ul>
 			<ul className="flex flex-col place-items-center">
-				<Link href={"https://github.com/Darksw20"}>
-					<Image
-						className="m-2"
-						src="/img/github.png"
-						width={50}
-						height={50}
-						alt="Github"
-					/>
-				</Link>
+				{userInfo?.github ? (
+					<Link href={userInfo.github}>
+						<Image
+							className="m-2"
+							src="/img/github.png"
+							width={50}
+							height={50}
+							alt="Github"
+						/>
+					</Link>
+				) : null}
 
-				<Link href={"https://www.linkedin.com/in/ricardonavi/"}>
-					<Image
-						className="m-2"
-						src="/img/linkedin.png"
-						width={50}
-						height={50}
-						alt="Linkedin"
-					/>
-				</Link>
+				{userInfo?.linkedin ? (
+					<Link href={userInfo.linkedin}>
+						<Image
+							className="m-2"
+							src="/img/linkedin.png"
+							width={50}
+							height={50}
+							alt="Linkedin"
+						/>
+					</Link>
+				) : null}
 			</ul>
 		</aside>
 	);
