5ab002e1a850a1ca6d11b037eb15a56e2be4d0a5
Darksw20 <ricardog.navi20@gmail.com>
2024-11-27 22:15:28 -0600

    Added username definition

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -5,6 +5,7 @@ import styles from "@/styles/Home.module.css";
 import { Home, Person, Settings, Work, Phone } from "@mui/icons-material";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,26 +13,35 @@ export default function Navbar() {
 	const router = useRouter();
 	const user = router.query?.user as string | undefined;
 	console.log("nvar user", user);
+	const [username, setUsername] = useState(user);
+
+	useEffect(() => {
+		if (router.isReady) {
+			const profile =
+				user || (process.env.NEXT_PUBLIC_DEFAULT_PROFILE as string);
+			setUsername(profile);
+		}
+	}, [router.isReady, user]);
 
 	return (
 		<aside className="container py-6 flex flex-col sidebar bg-neutral-900 grid-rows-3 justify-between w-16 h-screen sticky top-0 ">
 			<div className="flex flex-col place-items-center">
-				<Link href="/">
+				<Link href={username ? `/${username}/home` : "/"}>
 					<Home fontSize="large" />
 				</Link>
 			</div>
 
 			<ul className="flex flex-col place-items-center">
-				<Link href="/about">
+				<Link href={username ? `/${username}/about` : "/about"}>
 					<Person className="m-2" fontSize="large" />
 				</Link>
-				<Link href="/skills">
+				<Link href={username ? `/${username}/skills` : "/skills"}>
 					<Settings className="m-2" fontSize="large" />
 				</Link>
-				<Link href="/work">
+				<Link href={username ? `/${username}/work` : "/work"}>
 					<Work className="m-2" fontSize="large" />
 				</Link>
-				<Link href="/contact">
+				<Link href={username ? `/${username}/contact` : "/contact"}>
 					<Phone className="m-2" fontSize="large" />
 				</Link>
 			</ul>
